49d6b53d2fa0a85a818faf766553e3f71827c786
MrSametBurgazoglu <s.burgazoglu@gmail.com>
2023-06-30 10:17:21 +0300

    base url bugfix

--- NetworkManager/NetworkManager.go
+++ NetworkManager/NetworkManager.go
@@ -8,12 +8,54 @@ import (
 )
 
 type NetworkManager struct {
-	client  *http.Client
-	BaseUrl string
+	client     *http.Client
+	BaseUrl    string
+	CurrentUrl string
 }
 
 func (receiver *NetworkManager) Initialize() {
 	receiver.client = new(http.Client)
+}
+
+func (receiver *NetworkManager) GetPage(url string) []byte {
+	if strings.HasPrefix(url, "http") {
+		firstIndex := strings.Index(url, "//")
+		if firstIndex == -1 {
+			firstIndex = 0
+		}
+		index := strings.Index(url[firstIndex+2:], "/")
+		if index == -1 {
+			receiver.BaseUrl = url
+		} else {
+			receiver.BaseUrl = url[:index+firstIndex]
+		}
+		receiver.CurrentUrl = url
+	} else if strings.HasPrefix(url, "www") {
+		index := strings.Index(url, "/")
+		if index == -1 {
+			receiver.BaseUrl = url
+		} else {
+			receiver.BaseUrl = url[:index]
+		}
+		receiver.CurrentUrl = url
+	}
+	req, err := http.NewRequest("GET", receiver.CurrentUrl, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Set("User-Agent", "GezginWebBrowserEngine")
+
+	resp, err := receiver.client.Do(req)
+	if err != nil {
+		println(err.Error())
+		panic("wow there is problem")
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	println(body)
+	return body
 }
 
 func (receiver *NetworkManager) Get(url string) []byte {
@@ -25,11 +67,12 @@ func (receiver *NetworkManager) Get(url string) []byte {
 	} else {
 		index := strings.LastIndexAny(receiver.BaseUrl, "/")
 		if index == -1 {
-			targetUrl = receiver.BaseUrl
+			targetUrl = receiver.CurrentUrl + "/" + url
 		} else {
-			targetUrl = receiver.BaseUrl[:index]
+			targetUrl = receiver.CurrentUrl[:index] + url
 		}
 	}
+	println(targetUrl, "target url")
 	req, err := http.NewRequest("GET", targetUrl, nil)
 	if err != nil {
 		log.Fatalln(err)
